c7d4ad6d46b6771ca03b5411cdc7752703672216
Isi <isinsemail@gmail.com>
2025-08-28 10:45:23 +0100

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,5 @@
+from .delete_model_passthrough import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
+
+print("ComfyUI_DeleteModelPassthrough node loaded: Delete Model (Passthrough Any)")
